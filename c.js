0b2a53ef34a17e5667e897c8a7a11129a5204383
りょうや <127639508+kumaryoya@users.noreply.github.com>
2023-06-13 01:08:05 +0900

    Update c.js

--- c.js
+++ c.js
@@ -1,15 +1,16 @@
-const fetchRepositories = async () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const fetchRepositories = async () => {
   const githubName = document.querySelector("#github-name").value
   try {
     const res = await axios.get(`https://api.github.com/users/${githubName}/repos`)
-
     const repositoryLists = document.querySelector("#repository-lists")
     repositoryLists.innerHTML = res.data.map(repository => {
       return `<li><a href="${repository.html_url}">${repository.name}</a></li>`
     }).reduce((a, b) => {
       return a + b
     })
-  } catch {
+    } catch {
     alert("エラ〜")
+    }
   }
-}
+};
